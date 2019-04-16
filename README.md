ed945b3574c4dcfde1b47c675df47a227aef8977
Truc D T Nguyen <truc.ndtt@gmail.com>
2019-04-16 10:48:03 -0400

    Start remote peers Update readme

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-# To compile
+# Student info
+1. Truc D Nguyen (9482-7764)
+2. Vu Nhat Minh (or Minh Vu) (9194-1842)
+3. Ming-Hung Yu (3511-2299)
+
+# Compilation
 ```
 $ cd src/
 $ make
 ```
 
-# To run
+# Execute
 ```
 $ cd src/
 $ java peerProcess <peerID>
 ```
+
+# Start remote peers
+**Must give authorization without password first**
+```
+$ cd src/
+$ java StartRemotePeers <username> <working_dir>
+```
+- username: username for ssh
+- working_dir: path to **src/** on the remote machines
---
+++ src/StartRemotePeers.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * The StartRemotePeers class begins remote peer processes.
+ * It reads configuration file PeerInfo.cfg and starts remote peer processes.
+ */
+public class StartRemotePeers
+{
+
+    /**
+     * Store id, port, address of remote peerId
+     */
+    class RemotePeerInfo
+    {
+        public String peerId;
+        public String peerAddress;
+        public String peerPort;
+
+        public RemotePeerInfo(String pId, String pAddress, String pPort)
+        {
+            peerId = pId;
+            peerAddress = pAddress;
+            peerPort = pPort;
+        }
+    }
+
+    public Vector<RemotePeerInfo> peerInfoVector;
+
+    /**
+     * Read PeerInfo.cfg
+     */
+    public void getConfiguration()
+    {
+        String st;
+        peerInfoVector = new Vector<>();
+        try
+        {
+            BufferedReader in = new BufferedReader(new FileReader("PeerInfo.cfg"));
+            while ((st = in.readLine()) != null)
+            {
+
+                String[] tokens = st.split("\\s+");
+
+                peerInfoVector.addElement(new RemotePeerInfo(tokens[0], tokens[1], tokens[2]));
+            }
+
+            in.close();
+        } catch (Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+    }
+
+    /**
+     * Start remote peers at specified ssh username and path to src/
+     * @param args username and path
+     */
+    public static void main(String[] args)
+    {
+        try
+        {
+            StartRemotePeers myStart = new StartRemotePeers();
+            myStart.getConfiguration();
+
+            // get current path
+            String path = System.getProperty("user.dir");
+            String user = "trnguyen";
+
+            if (args.length > 0)
+            {
+                user = args[0];
+                if (args.length > 1)
+                    path = args[1];
+            } else
+            {
+                System.out.println("No command line arguments found");
+            }
+
+            // start clients at remote hosts
+            for (int i = 0; i < myStart.peerInfoVector.size(); i++)
+            {
+                RemotePeerInfo pInfo = myStart.peerInfoVector.elementAt(i);
+
+                System.out.println("Start remote peer " + pInfo.peerId + " at " + pInfo.peerAddress);
+
+                Runtime.getRuntime().exec("ssh " + user + "@" + pInfo.peerAddress + " cd " + path +
+                        "&& make && java peerProcess " + pInfo.peerId);
+            }
+            System.out.println("Starting all remote peers has done.");
+
+        } catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
+
+}
